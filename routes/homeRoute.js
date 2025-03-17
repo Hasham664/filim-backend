aa709287198ee4c67935a6cc46c404a5cd4534b4
Hasham664 <m.hasham3222@gmail.com>
2025-03-17 14:59:40 +0500

    push

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createGetHome, createHomePage } from '../controllers/HomeController.js';
+import { createGetHome, createHomePage } from '../controllers/homeController.js';
 import upload from '../middlewere/multer.js';
 
 
